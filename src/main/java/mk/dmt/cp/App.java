769e23a8497b4486946f5ad042269f1ff32c6da4
vdimitriev <vdimitriev@proton.me>
2024-07-28 11:24:50 +0200

    Initial commit

---
+++ src/main/java/mk/dmt/cp/App.java
@@ -0,0 +1,13 @@
+package mk.dmt.cp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
